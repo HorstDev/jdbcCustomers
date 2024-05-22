c51d7362f7335ef9cabe18f15c9cd99b79a63ef5
HorstDev <gorstsergei@mail.ru>
2024-05-22 17:58:10 +0400

    create dbConnection (jdbc)

---
+++ src/test/java/com/example/jdbcCustomers/JdbcCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbcCustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
